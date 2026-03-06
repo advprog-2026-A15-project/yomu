a1a8b4b105620b31198e4b4401ac651346addad8
Christna Yosua Rotinsulu <yosuamic123@gmail.com>
2026-03-06 15:56:11 +0700

    Add unit testing for config authentication

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.mockito:mockito-core")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
